a2a864b29533b35faf1793b35d8f674f123d2192
iofjuupasli <iofjuupasli@gmail.com>
2014-12-29 03:39:05 +0300

    init; server init

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var config = require('config');
+var express = require('express');
+
+var app = express();
+
+app.use(require('./config/nocache'));
+
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.use(function (err, req, res, next) {
+    console.error(err);
+    next(err);
+});
+
+app.listen(config.get('port'), function () {
+    console.log('deployed on port: ' + config.get('port'));
+});
