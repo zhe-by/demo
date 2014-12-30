f7c5fbc1856c29f76652233c1b3e08244cae9880
iofjuupasli <iofjuupasli@gmail.com>
2014-12-30 13:57:12 +0300

    response by landing by default

--- server/app.js
+++ server/app.js
@@ -6,12 +6,17 @@ var app = express();
 
 app.use(require('./config/nocache'));
 
-app.use(express.static(path.join(__dirname, '../frontend/static')));
 
 app.use(function (err, req, res, next) {
     console.error(err);
     next(err);
 });
+
+app.get('/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../frontend/static/landing.html'));
+});
+
+app.use(express.static(path.join(__dirname, '../frontend/static')));
 
 app.listen(config.get('port'), function () {
     console.log('deployed on port: ' + config.get('port'));
